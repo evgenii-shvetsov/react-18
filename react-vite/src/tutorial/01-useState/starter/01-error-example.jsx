789d45795e8676e92e703b81a6ac15a488773632
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-20 10:48:56 -0700

    useState hooks

--- react-vite/src/tutorial/01-useState/starter/01-error-example.jsx
+++ react-vite/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [count, setCount] = useState(0);
+
+  const increaseHandler = () => {
+    setCount((prev) => (prev += 1));
+  };
+  const decreaseHandler = () => {
+    setCount((prev) => (prev -= 1));
+  };
+  return (
+    <div>
+      <h2>useState error example</h2>
+      <h2>{count}</h2>
+      <button type="button" className="btn" onClick={increaseHandler}>
+        increase value
+      </button>
+      <button type="button" className="btn" onClick={decreaseHandler}>
+        decrease value
+      </button>
+    </div>
+  );
 };
 
 export default ErrorExample;
--- react-vite/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ react-vite/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,40 @@
+import { data } from "../../../data.js";
+import { useState } from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    const newArr = people.filter((person) => id !== person.id);
+    setPeople(newArr)
+  };
+
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div>
+      <h2>useState array example</h2>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type="button" onClick={()=> removeItem(id)}> remove</button>
+          </div>
+        );
+      })}
+      <button
+        type="button"
+        style={{ marginTop: "2rem" }}
+        onClick={clearAllItems}
+        className="btn"
+      >
+        clear items
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
