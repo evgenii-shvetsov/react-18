5e1a9b7b1a8353c9a19f63c5d7c12bfa7170b94b
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-20 14:20:05 -0700

    complete useeffect basics exercise

--- react-vite/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ react-vite/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+
   const sayHello = () => {
     console.log('hello there');
   };
 
-  sayHello();
+  useEffect(()=>{
+    sayHello();
+  },[])
 
   return (
     <div>
