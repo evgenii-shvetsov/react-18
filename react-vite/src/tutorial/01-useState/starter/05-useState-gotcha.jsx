e231c54626192f13f35d15f2dc8ac49e98ab6393
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-20 12:41:02 -0700

    complete useState practice

--- react-vite/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ react-vite/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setTimeout(() => setValue((prev) => (prev += 1)), 3000);
+  };
+
+  return (
+    <>
+      <h2>useState "gotcha"</h2>
+      <p>{value}</p>
+      <button onClick={handleClick}>increase</button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
