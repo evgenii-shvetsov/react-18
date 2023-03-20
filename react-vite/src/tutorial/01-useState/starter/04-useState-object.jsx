75034552cb7431082f3693fb8bb97e24fb6d826e
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-20 12:18:57 -0700

    practice useState hook with an object

--- react-vite/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ react-vite/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    hobby: "Bouldering",
+  });
+
+  const setupPerson = () => {
+    setPerson({ name: "Joe", age: 30, hobby: "Climbing" });
+    //if we want to changenot all properties, destructure first, then setup new key-value pair
+    setPerson({ ...person, name: "Bob" });
+  };
+
+  return (
+    <>
+      <h2>useState object example</h2>
+      <div>
+        <h3>{person.name}</h3>
+        <h3>{person.age}</h3>
+        <h3>Enjoys: {person.hobby}</h3>
+        <button type="button" onClick={setupPerson} className="btn">
+          Show Joe
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default UseStateObject;
