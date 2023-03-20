f864dc678049c9c626829092b6aa06d537119c19
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-20 09:24:50 -0700

    setup vite project

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,11 +1,9 @@
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return <h1>first vite project</h1>
+  return (
+    <div className='container'>
+      <h2>Advanced React</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
