4e9eaf805601d4ebe9558e53a9dac8e7a9bf48d9
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-20 23:14:15 -0700

    solve fetch github data challenge

--- react-vite/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ react-vite/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,43 @@
-const url = 'https://api.github.com/users';
+import { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // fetch(url)
+    //   .then((response) => response.json())
+    //   .then((data) => setUsers(data));
+    // console.log(users);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2>fetch data example</h2>
+      {users.map((user) => {
+        const { id, login, avatar_url, html_url } = user;
+        return (
+          <div key={id}>
+            <img src={avatar_url} alt="" style={{ width: "70px" }} />
+            <div>
+              <h5>{login}</h5>
+              <a href={html_url}>profile</a>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 export default FetchData;
