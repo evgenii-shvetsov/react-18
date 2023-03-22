29d1a9bc045f3fca6bdea09078a2ed51c0399b28
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-21 18:41:29 -0700

    solve toggle challenge

--- react-vite/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ react-vite/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [value, setValue] = useState(false)
+
+  const toggleBtn = () => {
+    setValue(prev => !prev)
+  }
+
+  return (
+    <>
+    <h2>toggle challenge</h2>
+    <button className="btn" onClick={toggleBtn}>Toggle Alert</button>
+    {value && <div>
+      you clicked the button
+    </div> }
+    </>
+  )
+  
 };
 
 export default ToggleChallenge;
