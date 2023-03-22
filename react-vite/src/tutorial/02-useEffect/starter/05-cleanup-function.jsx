3840101272d3407a6da604e05af676ac51761045
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-21 19:20:49 -0700

    add clean up function in useeffect

--- react-vite/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ react-vite/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+
+  const toggleFunc = () =>{
+    setToggle(prev => !prev)
+  }
+  return (
+    <>
+      <h2>cleanup function</h2>
+      <button className="btn" onClick={toggleFunc}>toggle component</button>
+      {toggle && <RandomComomponent />}
+    </>
+  );
 };
+
+const RandomComomponent = ()=>{
+  useEffect(()=>{
+      console.log("This is interesting")
+      const intId = setInterval(()=>{
+        console.log("Hello from interval");
+      },1000)
+      return ()=>{
+        clearInterval(intId)
+      }
+  },[])
+  return <h1>hello there</h1>
+}
 
 export default CleanupFunction;
