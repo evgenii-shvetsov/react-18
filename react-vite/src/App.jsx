0c34ea24c78d53b9d7f59642c79def4036487470
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-21 21:02:39 -0700

    setup project structure

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,11 +1,10 @@
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
-import Final from "./tutorial/02-useEffect/final/05-cleanup-function";
+import Example from "./tutorial/04-project-structure/starter/Example";
+
 
 function App() {
   return (
-    <div className='container'>
-      <Starter />
-      {/* <Final/> */}
+    <div className="container">
+      <Example/>
     </div>
   );
 }
