bd4b936f2d2123e2b787732fd58d204995536c3b
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-24 15:08:54 -0700

    add checkboxes and select inputs

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/06-forms/starter/03-multiple-inputs'
-import Final from "./tutorial/06-forms/final/03-multiple-inputs";
+import Starter from './tutorial/06-forms/starter/04-other-inputs'
+import Final from "./tutorial/06-forms/final/04-other-inputs";
 
 function App() {
   return (
