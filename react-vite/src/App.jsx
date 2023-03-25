e5abf7ea90c380143c4808f2fda76e6b003d4a34
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-25 13:10:20 -0700

    add custom hooks

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/07-useRef/starter/01-useRef-basics'
-import Final from "./tutorial/07-useRef/final/01-useRef-basics";
+import Starter from './tutorial/08-custom-hooks/starter/01-toggle'
+import Final from "./tutorial/08-custom-hooks/final/01-toggle";
 
 function App() {
   return (
