90564a6c5c72a36d3f9539904aa1febe0ece477f
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-24 17:28:22 -0700

    add uncontrolled inputs with form data

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/06-forms/starter/04-other-inputs'
+import Starter from './tutorial/06-forms/starter/05-form-data'
 import Final from "./tutorial/06-forms/final/04-other-inputs";
 
 function App() {
--- react-vite/src/tutorial/06-forms/starter/05-form-data.jsx
+++ react-vite/src/tutorial/06-forms/starter/05-form-data.jsx
@@ -5,11 +5,20 @@ const UncontrolledInputs = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    // console.log(formData)
+    // const name = formData.get('name')
+    // console.log([...formData.entries()])
+    const newUser = Object.fromEntries(formData)
+    console.log(newUser);
+    setValue(value + 1);
+    e.currentTarget.reset();
   };
   return (
     <div>
       <form className='form' onSubmit={handleSubmit}>
         <h4>Form Data API</h4>
+
         {/* name */}
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
@@ -17,6 +26,7 @@ const UncontrolledInputs = () => {
           </label>
           <input type='text' className='form-input' id='name' name='name' />
         </div>
+
         {/* email */}
         <div className='form-row'>
           <label htmlFor='email' className='form-label'>
@@ -24,7 +34,8 @@ const UncontrolledInputs = () => {
           </label>
           <input type='email' className='form-input' id='email' name='email' />
         </div>
-        {/* email */}
+        {/* password */}
+
         <div className='form-row'>
           <label htmlFor='password' className='form-label'>
             Password
