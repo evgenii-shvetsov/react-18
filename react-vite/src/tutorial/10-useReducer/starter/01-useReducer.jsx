32bd0e45e64f5bb78bd4cbb9305f37a5ca60cad7
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-26 11:38:07 -0700

    split actions and reducers in different folders

--- react-vite/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ react-vite/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,27 +1,14 @@
 import { useState, useReducer } from "react";
 import { data } from "../../../data";
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from "./actions";
+import reducer from "./reducer";
 
-const CLEAR_LIST = "CLEAR_LIST";
-const RESET_LIST = "RESET_LIST";
-const REMOVE_ITEM = "REMOVE_ITEM";
 
 const defaultState = {
   people: data,
 };
 
-const reducer = (state, action, payload) => {
-  if (action.type === CLEAR_LIST) {
-    return { ...state, people: [] };
-  }
-  if (action.type === RESET_LIST) {
-    return { ...state, people: data };
-  }
-  if (action.type === REMOVE_ITEM) {
-    let newArr = state.people.filter(person => person.id !== action.payload.id);
-    return {...state, people:newArr}
-  }
-  throw new Error(`No matching "${action.type}" - action type`);
-};
+
 
 const ReducerBasics = () => {
   const [state, dispatch] = useReducer(reducer, defaultState);
