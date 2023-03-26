47c9dd3eb9db49816a9b08b5e80520b3c4dcd4c4
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-26 12:39:11 -0700

    improved performance by abstracting components

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/10-useReducer/starter/01-useReducer'
-import Final from "./tutorial/08-custom-hooks/final/02-fetch-data";
+import Starter from './tutorial/11-performance/starter/02-lower-state-challenge'
+import Final from "./tutorial/11-performance/final/02-lower-state-challenge";
 
 function App() {
   return (
