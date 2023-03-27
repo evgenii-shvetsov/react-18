e1a2f46655726b0448f20915b1b9e09c4b0161a4
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-26 20:42:20 -0700

    create whole redux cycle

--- redux-toolkit/starter/src/index.js
+++ redux-toolkit/starter/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { store } from './store';
+import {Provider} from 'react-redux';
 import App from './App';
 
 const container = document.getElementById('root');
@@ -8,6 +10,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
